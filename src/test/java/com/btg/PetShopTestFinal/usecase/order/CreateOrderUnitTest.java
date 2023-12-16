b850ee7c5b604c08680dbf4d045f29b0c88b043e
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-12-16 10:51:41 -0300

    Update

--- src/test/java/com/btg/PetShopTestFinal/usecase/order/CreateOrderUnitTest.java
+++ src/test/java/com/btg/PetShopTestFinal/usecase/order/CreateOrderUnitTest.java
@@ -1,4 +1,81 @@
 package com.btg.PetShopTestFinal.usecase.order;
 
+import com.btg.PetShopTestFinal.modules.costumers.entity.Customer;
+import com.btg.PetShopTestFinal.modules.costumers.repository.CustomerRepository;
+import com.btg.PetShopTestFinal.modules.order.dto.OrderRequest;
+import com.btg.PetShopTestFinal.modules.order.dto.OrderResponse;
+import com.btg.PetShopTestFinal.modules.order.entity.Order;
+import com.btg.PetShopTestFinal.modules.order.repository.OrderRepository;
+import com.btg.PetShopTestFinal.modules.order.usecase.ExecuteOrder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
 public class CreateOrderUnitTest {
+
+    @InjectMocks
+    private ExecuteOrder createOrder;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    private List<Customer> customer;
+
+
+    @BeforeEach
+    public void setUp() {
+        customer = (List<Customer>) new Customer();
+        when(customerRepository.findByName(Mockito.any())).thenReturn((Customer) customer);
+
+    }
+
+    @Test
+    public void testInvalidCustomerMustReturnException() {
+
+
+        when(customerRepository.findByName(Mockito.any())).thenReturn(null);
+
+        assertThrows(IllegalStateException.class,
+                () -> createOrder.execute((OrderRequest) customer));
+    }
+
+    @Test
+    public void testExecuteOrderSuccessfully() throws Exception {
+
+        OrderRequest orderRequest = new OrderRequest();
+        Customer mockCustomer = new Customer();
+        when(customerRepository.findByIdTransaction(orderRequest.getCustomerId())).thenReturn(mockCustomer);
+
+        OrderResponse orderResponse = createOrder.execute(orderRequest);
+
+        assertNotNull(orderResponse);
+
+        verify(orderRepository, times(1)).save(any(Order.class));
+    }
+
+    @Test
+    public void testExecuteOrderCustomerNotFound() {
+
+        OrderRequest orderRequest = new OrderRequest();
+        when(customerRepository.findByIdTransaction(orderRequest.getCustomerId())).thenReturn(null);
+
+        assertThrows(Exception.class, () -> createOrder.execute(orderRequest));
+
+        verify(orderRepository, never()).save(any(Order.class));
+    }
+
 }
--- src/test/java/com/btg/PetShopTestFinal/usecase/order/DeleteOrderUnitTest.java
+++ src/test/java/com/btg/PetShopTestFinal/usecase/order/DeleteOrderUnitTest.java
@@ -1,4 +1,53 @@
 package com.btg.PetShopTestFinal.usecase.order;
 
+import com.btg.PetShopTestFinal.modules.order.entity.Order;
+import com.btg.PetShopTestFinal.modules.order.repository.OrderRepository;
+import com.btg.PetShopTestFinal.modules.order.usecase.DeleteOrder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
 public class DeleteOrderUnitTest {
+    @InjectMocks
+    private DeleteOrder deleteOrder;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testExecuteOrderSuccessfully() throws Exception {
+
+        String orderId = "123";
+        Order mockOrder = new Order();
+        when(orderRepository.findOrderById(orderId)).thenReturn(mockOrder);
+
+        deleteOrder.execute(orderId);
+
+        verify(orderRepository, times(1)).delete(mockOrder);
+    }
+
+    @Test
+    public void testExecuteOrderNotFound() {
+
+        String orderId = "456";
+        when(orderRepository.findOrderById(orderId)).thenReturn(null);
+
+        assertThrows(Exception.class, () -> deleteOrder.execute(orderId));
+
+        verify(orderRepository, never()).delete(any(Order.class));
+    }
+
 }
